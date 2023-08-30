4a18f81a8e331cd8cf0cc183dd13d28d4605ccf9
JayaprakashGedela <111943055+JayaprakashGedela@users.noreply.github.com>
2023-08-30 15:05:14 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package com.testappu.todolistapplicationmlh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList <String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+}
+
+
